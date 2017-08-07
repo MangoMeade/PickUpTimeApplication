88afa3c07ce8157b42558850500e84d74be91450
chandlerkeyes <ckeyes4@emich.edu>
2017-08-07 15:32:41 -0400

    Added hibernate

--- src/main/java/com/jdbc/controller/HomeController.java
+++ src/main/java/com/jdbc/controller/HomeController.java
@@ -1,7 +1,15 @@
 package com.jdbc.controller;
+        import com.test.models.UsersEntity;
+        import org.hibernate.Criteria;
+        import org.hibernate.Session;
+        import org.hibernate.SessionFactory;
+        import org.hibernate.cfg.Configuration;
         import org.springframework.stereotype.Controller;
         import org.springframework.web.bind.annotation.RequestMapping;
         import org.springframework.web.servlet.ModelAndView;
+
+        import java.util.ArrayList;
+
 @Controller
 public class HomeController {
     //this calls the method
@@ -12,4 +20,5 @@ public class HomeController {
                 //the type is model and view which brings together model and view
                 ModelAndView("welcome","message","Welcome to GC Coffee");
     }
+
 }
