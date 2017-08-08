81b8599d552f7dac9bb93c88870eb478ac838a34
chandlerkeyes <ckeyes4@emich.edu>
2017-08-08 10:59:11 -0400

    Added a controller class for login, took Request mappings for isignup page and login and placed it into LoginControler

--- src/main/java/com/jdbc/controller/HomeController.java
+++ src/main/java/com/jdbc/controller/HomeController.java
@@ -31,60 +31,7 @@ public class HomeController {
         Session selectAll = sessionFact.openSession();
         selectAll.beginTransaction();
         return selectAll;
-    }
 
-    @RequestMapping("/")
-    public ModelAndView loginPage() {
-        return new
-                //the type is model and view which brings together model and view
-                ModelAndView("login", "loginPage", "login");
-    }
-
-    @RequestMapping("/seeEvents")
-    public String function(Model model, @RequestParam("username") String username,
-                           @RequestParam("password") String password) {
-
-        model.addAttribute("username", username);
-        model.addAttribute("password", password);
-
-        return "seeEvents";
-    }
-
-    @RequestMapping("/getNewUser")
-
-    public String newCustomer() {
-
-        return "adduserform";
-
-    }
-
-    @RequestMapping("/addusersuccess")
-
-    public String addNewUser(@RequestParam("firstName") String firstname,
-                             @RequestParam("lastName") String lastname,
-                             @RequestParam("email") String email,
-                             @RequestParam("phoneNumber") String phoneNum,
-                             @RequestParam("gender") String gender,
-                             @RequestParam("userName") String username,
-                             @RequestParam("password") String password,
-                             Model model) {
-        Configuration cfg = new Configuration().configure("hibernate.cfg.xml");
-        SessionFactory sessionFact = cfg.buildSessionFactory();
-        Session session = sessionFact.openSession();
-        Transaction tx = session.beginTransaction();
-        UsersEntity newUsers = new UsersEntity();
-        newUsers.setFirstName(firstname);
-        newUsers.setLastName(lastname);
-        newUsers.setEmail(email);
-        newUsers.setPhoneNumber(phoneNum);
-        newUsers.setGender(gender);
-        newUsers.setUserName(username);
-        newUsers.setPassword(password);
-        session.save(newUsers);
-        tx.commit();
-        session.close();
-        model.addAttribute("NewUsers", newUsers);
-        return "addusersuccess";
     }
 
     private ParentUserDao userDao = DaoUserFactory.getDaoInstance(ParentUserDao.HIBERNATE_DAO);
