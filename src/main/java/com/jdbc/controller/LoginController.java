06e67168c283d03d85ab91a097bf9eab2ed3d7a0
chandlerkeyes <ckeyes4@emich.edu>
2017-08-08 11:08:40 -0400

    changes to Login Controller

--- src/main/java/com/jdbc/controller/LoginController.java
+++ src/main/java/com/jdbc/controller/LoginController.java
@@ -5,10 +5,13 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+
+@Controller
 
 public class LoginController {
     @RequestMapping("/")
